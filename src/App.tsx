4b8bcf5dbea050abfdf9eeeca150e126081b712e
Kirill <publickirill2245@gmail.com>
2025-05-13 14:30:28 +0500

    firts

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css'
+  const App: React.FC = () => {
+    return (
+      <div>
+        <h1 className='title'>Hello, World!</h1>
+      </div>
+    );
+  };
+  export default App;
